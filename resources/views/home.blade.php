8d4dac8a6358c647468811bc7d911b5874925060
Douglas Black <dougblackjr@gmail.com>
2018-05-01 19:06:23 -0400

    Adding npm stuff, laradock, adjusting readme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Calendar</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <main id="calendar"></main>
                 </div>
             </div>
         </div>
