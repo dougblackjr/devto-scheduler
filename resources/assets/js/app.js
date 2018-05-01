3676afaf1facc77d03b76f253a3167f7bf8ce318
Douglas Black <dougblackjr@gmail.com>
2018-05-01 19:10:34 -0400

    Fixing babel error

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,12 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import FullCalendar from 'vue-full-calendar'
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.use(FullCalendar)
 
 const app = new Vue({
     el: '#app'
 });
+
