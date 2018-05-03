9bf2f2ba596780f5c95dab69e65f85aef8593cb0
Douglas Black <dougblackjr@gmail.com>
2018-05-02 20:04:36 -0400

    Adding initial events controller

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -30,10 +30,26 @@ class AppointmentController extends Controller
 
 	}
 
-	public function index()
+	public function index(Request $request)
 	{
 
-		$resources = Resource::pluck('id', 'title')->get();
+		$outputData = [];
+
+		$resources = Appointment::whereBetween('start', [$request->startDate, $request->endDate])
+								->get();
+
+		$resources->each(function ($r) use ($outputData) {
+
+			$outputData[] = array(
+				'id' => $r->id,
+				'resourceId' => $r->resource_id,
+				'title' => $r->title,
+				'description' => $r->description,
+				'start' => $r->start,
+				'end' => $r->end
+			);
+
+		});
 
 		return response()->json($resources);
 
