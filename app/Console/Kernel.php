7e1ed11be38a05af60c7a0e796f257a18125b38d
Douglas Black <dougblackjr@gmail.com>
2018-05-08 04:33:43 -0400

    Adding command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        ResetEverythingCommand::class
     ];
 
     /**
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('devto:reset')
+                 ->hourly();
     }
 
     /**
