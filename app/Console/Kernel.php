9ff77d55f3b268d080cdcf8b673e5331ec27f6cc
Douglas Black <dougblackjr@gmail.com>
2018-05-08 09:44:19 -0400

    Fixing command namespace

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        ResetEverythingCommand::class
+        Commands\ResetEverythingCommand::class
     ];
 
     /**
